624bab34e77088eb81bd659333f94771db7bfee1
Mike <60325499+leguim-repo@users.noreply.github.com>
2023-07-03 20:10:59 +0200

    composerize containers

--- clear-notebooks-outputs.sh
+++ clear-notebooks-outputs.sh
@@ -1,4 +1,4 @@
 # https://nbconvert.readthedocs.io/en/latest/install.html
 # pip install nbconvert
 
-python3 -m nbconvert --clear-output code/*.ipynb code/**/*.ipynb
+python3 -m nbconvert --clear-output examples/*.ipynb examples/**/*.ipynb
